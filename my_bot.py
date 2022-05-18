2a4e2b76a3993e70e9b2a9a7295262fa1b4db92f
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2022-05-18 16:40:22 +0530

    first commit

---
+++ my_bot.py
@@ -0,0 +1,78 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+  
+updater = Updater("bot_token",
+                  use_context=True)
+  
+  
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Hello sir, Welcome to the Bot.Please write\
+        /help to see the commands available.")
+    print("bot started")
+  
+def help(update: Update, context: CallbackContext):
+    update.message.reply_text("""Available Commands :-
+    /youtube - To get the youtube URL
+    /linkedin - To get the LinkedIn profile URL
+    /gmail - To get gmail URL
+    /github - To get the Github profile URL
+    /instagram -To get the instagram url""")
+  
+  
+def gmail_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Your can contact through gmail \
+        jeevanjoseph1310@gmail.com")
+  
+  
+def youtube_url(update: Update, context: CallbackContext):
+    update.message.reply_text("Youtube Link =>\
+    https://www.youtube.com/")
+  
+  
+def linkedIn_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "LinkedIn URL => \
+        https://www.linkedin.com/in/jeevan-joseph-1310/ ")
+  
+  
+def github_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Github profile URL => https://github.com/jeevan1310")
+
+def instagram_url(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Instagram URL => \
+        https://instagram.com/jeevanjoseph1310 ")
+  
+  
+def unknown(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry '%s' is not a valid command" % update.message.text)
+  
+  
+def unknown_text(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Sorry I can't recognize you , you said '%s'" % update.message.text)
+  
+  
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('youtube', youtube_url))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(CommandHandler('linkedin', linkedIn_url))
+updater.dispatcher.add_handler(CommandHandler('gmail', gmail_url))
+updater.dispatcher.add_handler(CommandHandler('github', github_url))
+updater.dispatcher.add_handler(CommandHandler('instagram', instagram_url))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown))
+updater.dispatcher.add_handler(MessageHandler(
+    Filters.command, unknown))  # Filters out unknown commands
+  
+# Filters out unknown messages.
+updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+  
+updater.start_polling()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# My first Telegram bot
+
+This is the first telegram bot i am developing to learn how bot is worked inside telegram 
